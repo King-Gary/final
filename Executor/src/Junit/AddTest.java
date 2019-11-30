7f982dd032ef92fba330914e5bbec6c59399b4b2
Gary <Gary@DESKTOP-N5T8JS1.home>
2019-11-30 17:42:15 -0500

    junit

--- Executor/src/Junit/AddTest.java
+++ Executor/src/Junit/AddTest.java
@@ -5,6 +5,7 @@
  */
 package Junit;
 
+import org.junit.Assert;
 import static org.junit.Assert.*;
 
 
@@ -15,14 +16,31 @@ import org.junit.Test;
  * @author Gary
  */
 class AddTest {
+
+    private int newNumbers;
     
 	public void testAdd () {
 
 		junit test = new  junit();
 
 		int result = test.add(3, 4);
+                int nonTest = 3+7;
+		assertEquals(7, result);	
+                
+                int[] numbers = new int[] {1,2,3,4,5,6,7,8,9};
+                int[] tests = new int[] {1,2,3,4,5,6,7,8,9};
+                Assert.assertArrayEquals(numbers, tests);
+                
+                assertTrue("This is bound to succeed.", true);
+                assertTrue("This is doomed to fail!", false);
 
-		assertEquals(7, result);		
-
+                assertFalse("This is bound to succeed.", false);
+                assertFalse("This is doomed to fail!", true);
+                
+                assertNull("This message contains nothing", result);
+                assertNotNull("This message does contain something", result);
+                assertSame("These objects are the same", result, nonTest);
+                assertNotSame("These objects are not the same", result, nonTest);
+                
 	}
 }
